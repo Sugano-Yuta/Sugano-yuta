9824d22ccb2a268842dd7b5540e21e7ad6bd7ea3
tech-compass <techcompassmanagement@gmail.com>
2019-12-11 20:31:32 +0900

    課題　変数　task3

---
+++ variable/src/variable/task3.java
@@ -0,0 +1,25 @@
+package variable;
+
+public class task3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	int intNum=219;
+	long a=intNum;
+	System.out.println(a);
+	
+	double doubleNum=364.2;
+	float b=(float) doubleNum;
+	System.out.println(b);
+	long longNum=123456L;
+	double c=longNum;
+	System.out.println(c);
+	
+	
+
+	
+		
+	}
+
+}
