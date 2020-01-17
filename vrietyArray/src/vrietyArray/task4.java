a6bad5eed3a92c09f26da074e4ebe41cf71a8e45
tech-compass <techcompassmanagement@gmail.com>
2020-01-17 14:35:59 +0900

    色々な配列　課題4(漏れ)

--- vrietyArray/src/vrietyArray/task4.java
+++ vrietyArray/src/vrietyArray/task4.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 public class task4 {
 
 	public static void main(String[] args) {
-		
+
 		int [] array = new int [] {21,3,32,6,99,72,78,51,1,26,87,11,48,60,29,5,46,74,14,193,215,83};
 		ArrayList <Integer> odd = new ArrayList<>();
 		ArrayList <Integer> even = new ArrayList<>();
@@ -25,7 +25,7 @@ public class task4 {
 		for(int i = 0; i < even.size(); i++) {
 			evenHashMap.put(i,even.get(i));
 		}
-		System.out.println(evenHashMap.values());
-		System.out.println(oddHashMap);
+		System.out.println("{偶数" + evenHashMap.values() + "}");
+		System.out.println("{奇数" + oddHashMap.values() + "}");
 	}
 }
