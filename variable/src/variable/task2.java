7cb38f92c062829f28a9d484f953ba5c5e912016
tech-compass <techcompassmanagement@gmail.com>
2020-01-18 15:21:32 +0900

    変数　レビュー直し

--- variable/src/variable/task2.java
+++ variable/src/variable/task2.java
@@ -3,10 +3,9 @@ package variable;
 public class task2 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
-		System.out.println("私の名前は菅野優太です。");
+		final String MYNAME = "菅野優太";
+		System.out.println(MYNAME);
 		final String NUMBER ="0120-333-396";
 		System.out.println("\'" + NUMBER + "\'");
-		}
+	}
 }
--- variable/src/variable/task3.java
+++ variable/src/variable/task3.java
@@ -5,7 +5,7 @@ public class task3 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-		int intNum=219;
+		int intNum = 219;
 		long a = intNum;
 		System.out.println(a);
 		double doubleNum = 364.2;
