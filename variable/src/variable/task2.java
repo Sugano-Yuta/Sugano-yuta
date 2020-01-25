3e3550380975b9ad57be6119ac1238a858ce4960
tech-compass <techcompassmanagement@gmail.com>
2020-01-25 14:08:36 +0900

    変数　課題　レビュー直しレビュー直し

--- variable/src/variable/task2.java
+++ variable/src/variable/task2.java
@@ -3,9 +3,9 @@ package variable;
 public class task2 {
 
 	public static void main(String[] args) {
-		final String MYNAME = "菅野優太";
-		System.out.println(MYNAME);
+		String myname = "菅野優太";
+		System.out.println(myname + "です。");
 		final String NUMBER ="0120-333-396";
-		System.out.println("\'" + NUMBER + "\'");
+		System.out.println("私の電話番号は\'" + NUMBER + "\'です。");
 	}
 }
