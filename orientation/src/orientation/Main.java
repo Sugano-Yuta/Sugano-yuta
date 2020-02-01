89ed0486acad4f192adf2fd11cc123b034b10181
tech-compass <techcompassmanagement@gmail.com>
2020-02-01 14:04:57 +0900

    オブジェクト指向　課題　レビュー直し

---
+++ orientation/src/orientation/Main.java
@@ -0,0 +1,18 @@
+package orientation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Dog dog = new Dog("ポチ",5,12.3); //コンストラクタを呼び出し実行するためにnew ,()に引数を入れた
+		Animal animal = new Animal();
+
+		animal = dog;
+		animal.sleep();
+		dog.sleep();
+		dog.run();
+
+
+	}
+}
