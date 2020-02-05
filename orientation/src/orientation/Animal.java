1ccd57bd40bfdc88e81313085b2417f6d1c96e4d
tech-compass <techcompassmanagement@gmail.com>
2020-02-05 10:39:43 +0900

    オブジェクト指向　課題　レビュー直し

--- orientation/src/orientation/Animal.java
+++ orientation/src/orientation/Animal.java
@@ -7,18 +7,10 @@ public class Animal {
 	public Animal() {
 
 	}
-	public Animal(String name,int age) {
-		this.name = name;
-		this.age = age;
-	}
-
-//	System.out.println("名前:" + this.name);
+	//	System.out.println("名前:" + this.name);
 //	System.out.println("年齢:" + this.age + "歳");
 	public void sleep() {
 		System.out.println("眠った");
 	}
-	public void animalDate() {
-		System.out.println("名前:" + this.name);
-		System.out.println("年齢:" + this.age  + "歳");
-	}
 }
+	
