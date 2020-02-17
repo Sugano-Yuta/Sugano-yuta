9ae6558faa7c438d6b8b2fdadd9d3e063a11e069
tech-compass <techcompassmanagement@gmail.com>
2020-02-17 17:39:31 +0900

    オブジェクト指向　

--- instance/src/instance/task3.java
+++ instance/src/instance/task3.java
@@ -5,20 +5,13 @@ public class task3 {
 	public static void main(String[] args) {
 
 		// インスタンス生成
-		Person taro = new Person();
+		Person sugano = new Person();
 
 		// name, ageに値を入れる
-		taro.name = "太郎";
-		taro.age = 23;
+		sugano.name = "菅野優太";
+		sugano.age = 23;
 
 		// myPrifileを呼び出す
-		taro.myProfile();
-
-
-//			public person @myProfile(); {
-//				this.name = ("菅野優太");
-//				this.age = (23);
-//		}
-
+		sugano.myProfile();
 	}
 }
