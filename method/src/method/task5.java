c74244eeb45400a94732b14f3f6ff035aa721722
tech-compass <techcompassmanagement@gmail.com>
2020-02-22 15:47:10 +0900

    メソッド　レビュー直し

--- method/src/method/task5.java
+++ method/src/method/task5.java
@@ -1,21 +1,12 @@
 package method;
 
-import java.util.Arrays;
-
 public class task5 {
 
 	public static void main(String[] args) {
-		String [] array = {"null","nell","nill","nall","noll"};
-		task5(array);
-}
-
-	private static void task5(String[] array) {
-		if(Arrays.asList(array).contains("null")) {
-			System.out.println("true");
-			}else {
-				System.out.println("false");
-			}
 		
-	
+		String array [] = null;
+		if(array[0] == null) {
+			System.out.println(true);
+		}else System.out.println(false);
 	}
 }
