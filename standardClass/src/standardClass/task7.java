26a29123d8a0bcb2ec6484c6f8a8511d0f796816
tech-compass <techcompassmanagement@gmail.com>
2020-03-07 11:15:12 +0900

    標準クラス　レビュー直し

--- standardClass/src/standardClass/task7.java
+++ standardClass/src/standardClass/task7.java
@@ -26,20 +26,27 @@ public class task7 {
 		System.out.print(future.get(Calendar.DAY_OF_MONTH) + "日");
 		int dayofweek = future.get(Calendar.DAY_OF_WEEK);
 		switch(dayofweek){
-		case 1 :System.out.print("(日曜日)");
-		break;
-		case 2:System.out.print("(月曜日)");
-		break;
-		case 3:System.out.print("(火曜日)");
-		break;
-		case 4:System.out.print("(水曜日)");
-		break;
-		case 5:System.out.print("(木曜日)");
-		break;
-		case 6:System.out.print("(金曜日)");
-		break;
-		case 7:System.out.print("(土曜日)");
-		break;
+		case 1 :
+			System.out.print("(日曜日)");
+			break;
+		case 2:
+			System.out.print("(月曜日)");
+			break;
+		case 3:
+			System.out.print("(火曜日)");
+			break;
+		case 4:
+			System.out.print("(水曜日)");
+			break;
+		case 5:
+			System.out.print("(木曜日)");
+			break;
+		case 6:
+			System.out.print("(金曜日)");
+			break;
+		case 7:
+			System.out.print("(土曜日)");
+			break;
 		}
 	}
 }
