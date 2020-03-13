0da6677ec4764fc66c79132b68eb0af73191de93
tech-compass <techcompassmanagement@gmail.com>
2020-03-13 13:47:28 +0900

    メソッド　レビュー直し

--- method/src/method/task5.java
+++ method/src/method/task5.java
@@ -1,22 +1,30 @@
 package method;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class task5 {
 
 	public static void main(String[] args) {
 
-		String array1 [] = {"a","b","c"};
-		List<String> L1 = Arrays.asList(array1);
-		String array2 [] = {"a",null,"c"};
-		List<String> L2 = Arrays.asList(array2);
-		if(L1.contains(null)) {
-			System.out.println(true);
-		}else System.out.println(false);
-	if(L2.contains(null)) {
-		System.out.println(true);
-	}else System.out.println(false);
 
-}
+
+		String array1 [] = {"a","b","c"};
+		nullcheck();
+	//	List<String> L1 = Arrays.asList(array1);
+		String array2 [] = {"a",null,"c"};
+		nullcheck();
+	//	List<String> L2 = Arrays.asList(array2);
+	//	if(L1.contains(null)) {
+	//		System.out.println(true);
+	//	}else System.out.println(false);
+	//	if(L2.contains(null)) {
+	//		System.out.println(true);
+	//	}else System.out.println(false);
+
 	}
+
+
+	public static boolean nullcheck() {
+
+		return false;
+
+	}
+}
